f8f8ec8e29b3530a53f9d557887359a526211cc4
Agustin <rojasagustin90@gmail.com>
2019-08-27 22:37:29 -0300

    Update config.py

--- federated_trainer/config.py
+++ federated_trainer/config.py
@@ -1,4 +1,4 @@
-ACTIVE_ENCRYPTION = True
+ACTIVE_ENCRYPTION = False
 MAX_ITERATIONS = 100  # El numero de iteraciones aceptables utilizando PheEncryption por ahora es 4
 DATA_OWNER_PORT = 5000
 ITERS_UNTIL_PARTIAL_RESULT = 5
